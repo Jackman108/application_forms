639d5ae83bed0063b3f96f10553e27ecace08128
Jackman108 <dev.js.eugene@gmail.com>
2024-03-10 03:43:33 +0300

    Finalization of validation exceptions and destructuring

---
+++ resources/views/livewire/form-success.blade.php
@@ -0,0 +1,13 @@
+<!-- resources/views/livewire/form-success.blade.php -->
+<div class="mb-3">
+    <p>
+        Данные сотрудника: {{ $first_name }} {{ $last_name }} сохранены
+    </p>
+    <div class="alert alert-success">
+        Успешно.
+    </div>
+    <p>
+        Для отправки новой формы обновите страницу
+    </p>
+</div>
+
